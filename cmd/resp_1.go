13fc210acdac8758188df10f38fce319d121e39a
varun27896 <varun27896@gmail.com>
2023-10-16 23:50:26 -0500

    adds io reader

---
+++ cmd/resp_1.go
@@ -0,0 +1,23 @@
+const (
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
+)
+
+type Value struct {
+	typ   string
+	str   string
+	num   int
+	bulk  string
+	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
